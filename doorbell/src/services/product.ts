bcbd3fba5423c300a98e996e7f1c32899a3f324b
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2023-12-06 09:08:43 +0200

    Added the medusa experiment

---
+++ doorbell/src/services/product.ts
@@ -0,0 +1,9 @@
+import { 
+    ProductService as MedusaProductService,
+  } from "@medusajs/medusa"
+  
+  class ProductService extends MedusaProductService {
+    // TODO add customizations
+  }
+  
+  export default ProductService
